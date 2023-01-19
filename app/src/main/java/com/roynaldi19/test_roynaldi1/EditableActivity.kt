6ecfc14a077b297ed9cd855c9f1f43bf476bcffa
roynaldi19 <roynaldi19@gmail.com>
2023-01-19 15:01:05 +0700

    text send to firebase

--- app/src/main/java/com/roynaldi19/test_roynaldi1/EditableActivity.kt
+++ app/src/main/java/com/roynaldi19/test_roynaldi1/EditableActivity.kt
@@ -2,13 +2,18 @@ package com.roynaldi19.test_roynaldi1
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.roynaldi19.test_roynaldi1.databinding.ActivityEditableBinding
 
 class EditableActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityEditableBinding
+    private lateinit var db: DatabaseReference
     private val TAG = "EditableActivity"
+    private var id = 0
 
     companion object {
         const val EXTRA_TEXT = "extra_text"
@@ -23,6 +28,26 @@ class EditableActivity : AppCompatActivity() {
         Log.i(TAG, "edtResult $textReceived")
 
         binding.edtResult.setText(textReceived)
+
+        db = FirebaseDatabase.getInstance().getReference("text")
+
+        binding.btnSendToDatabase.setOnClickListener {
+            sendToDatabase()
+        }
+
+    }
+
+    private fun sendToDatabase() {
+        val textToDataBase = binding.edtResult.text.toString()
+        db.child(id.toString()).setValue(textToDataBase)
+            .addOnCompleteListener {
+                Toast.makeText(this@EditableActivity, "Text Send", Toast.LENGTH_SHORT).show()
+                binding.edtResult.text.clear()
+            }
+            .addOnFailureListener {
+                Toast.makeText(this@EditableActivity, "Failed Send", Toast.LENGTH_SHORT).show()
+
+            }
 
     }
 }
